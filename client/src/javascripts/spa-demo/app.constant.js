ef5e2c69deb542a76892317d2abcbe4088cf4eac
Dhinakaran Chinappen <dhinakaran.chinappen@gmail.com>
2018-01-02 08:47:53 -0500

    finished cities according to foos-ui - set 51, 13 to go

--- client/src/javascripts/spa-demo/app.constant.js
+++ client/src/javascripts/spa-demo/app.constant.js
@@ -5,6 +5,8 @@
         .module('spa-demo')
         .constant('spa-demo.APP_CONFIG', {
         	server_url: "http://localhost:3000",
-        	main_page_html: 'spa-demo/pages/main.html'
+        	main_page_html: 'spa-demo/pages/main.html',
+        	cities_html: "spa-demo/cities/cities.html"
+
         });
 })();
