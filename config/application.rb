7800dd3900915de2093aae5d2ceb4556600ecba3
Dhinakaran Chinappen <dhinakaran.chinappen@gmail.com>
2018-01-02 12:37:57 -0500

    added suggest CORS power

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,8 @@ module Module1
 
     config.middleware.insert_before 0, "Rack::Cors" do
         allow do
-            origins '*'
+            # origins '*'
+            origins /https:\/\/\w+\.github\.io/
 
             resource '/api/*',
             :headers => :any,
